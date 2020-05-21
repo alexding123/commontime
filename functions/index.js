90929c2628a191feebd0a89b7ba5df379cb0037d
Alex Ding <alexding123@outlook.com>
2020-05-21 15:40:21 -0400

    Add email support

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,16 @@ const functions = require('firebase-functions')
 
 /**
  * Set up Sentry for error reporting
+ * Disabled in development
  */
 if (functions.config().sentry && functions.config().sentry.dsn) {
-  Sentry.init({ dsn: functions.config().sentry.dsn })
+  const dsn = functions.config().sentry.dsn
+  
+  Sentry.init({
+    // using === because sometimes it might be a string
+    dsn: process.env.FUNCTIONS_EMULATOR === true ?
+         null : dsn,
+  })
 } else {
   console.warn(
     "/!\\ sentry.dsn environment variable not found. Skipping setting up Sentry..."
